a6b573978df106c3cacd05bc88902e2a1f0e0206
Will <willbickham3@gmail.com>
2022-07-13 17:17:41 -0400

    Name gridSize prompt;Write function name for createGrid; Add Create Grid button functionality

--- script.js
+++ script.js
@@ -4,5 +4,18 @@ const container = document.querySelector('.grid-container');
 // name variable to append later
 let newDiv = document.createElement('div');
 newDiv.className = 'square';
-
 container.appendChild(newDiv)
+
+// Name a variable to be used for size of grid
+gridSize = prompt('How big would you like your grid?')
+
+// Function to populate grid
+function createGrid() {
+
+}
+
+
+
+// Naming button to add event listener
+gridButton = document.querySelector('.create-grid');
+gridButton.addEventListener('click', createGrid)
