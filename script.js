1523ced4b5ac0c95132bb8d3c88814059cbd48de
Will <willbickham3@gmail.com>
2022-07-13 03:43:26 -0400

    Name variables for grid-container and creating divs

---
+++ script.js
@@ -0,0 +1,6 @@
+// name variable for container
+const container = document.querySelector('.grid-container');
+
+// name variable to append later
+let newDiv = document.createElement('div');
+newDiv.className = 'square';
