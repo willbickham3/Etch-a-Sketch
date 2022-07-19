73930e41673ad0a3476209e0fe103d7eaf10a329
Will <willbickham3@gmail.com>
2022-07-19 07:39:55 -0400

    Add ability to create different grids, deleting the old one

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const container = document.querySelector('.grid-container');
     // newDiv.className = 'square';
     // container.appendChild(newDiv)
 newDiv = "";
-
 // Name a variable to be used for size of grid
 const pixels = document.querySelector('#pixels');
 const gridButton = document.querySelector('.create-grid');
@@ -16,6 +15,8 @@ gridButton.addEventListener('click', createGrid);
 // Function to populate grid
 function createGrid() {
     gridSize = pixels.value;
+    let oldSquares = document.querySelectorAll('.square');
+    oldSquares.forEach((div) => div.remove());
     for (let i = 0; i < gridSize * gridSize; i++) {
     newDiv = document.createElement('div');
     newDiv.className = 'square';
@@ -23,9 +24,10 @@ function createGrid() {
     }
     container.style.gridTemplateColumns = `repeat(${gridSize}, auto)`;
     container.style.gridTemplateRows = `repeat(${gridSize}, auto)`;
-    gridButton.removeEventListener('click', createGrid);
+    //gridButton.removeEventListener('click', createGrid);
 }
 
+const myBtns = document.querySelectorAll('button');
 
 
 // Naming button to add event listener
