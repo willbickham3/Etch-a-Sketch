a023d933f31563626677d2aee8b2a4da7e806271
Will <willbickham3@gmail.com>
2022-07-19 08:08:28 -0400

    Add variable to select all divs in grid; Add function to change the color of mouseover

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // name variable for container
 const container = document.querySelector('.grid-container');
+const myBtns = document.querySelectorAll('button');
 
 // name variable to append later
     // let newDiv = document.createElement('div');
     // newDiv.className = 'square';
     // container.appendChild(newDiv)
 newDiv = "";
+let color = 'black';
 // Name a variable to be used for size of grid
 const pixels = document.querySelector('#pixels');
 const gridButton = document.querySelector('.create-grid');
@@ -24,12 +26,35 @@ function createGrid() {
     }
     container.style.gridTemplateColumns = `repeat(${gridSize}, auto)`;
     container.style.gridTemplateRows = `repeat(${gridSize}, auto)`;
-    //gridButton.removeEventListener('click', createGrid);
+    let gridSquares = document.querySelectorAll('div');
+    gridSquares.addEventListener('mouseover', colorGrid);
 }
 
-const myBtns = document.querySelectorAll('button');
+function colorGrid() {
+    switch (color) {
 
+    }
+     
+}
 
+function changeColor (event) {
+    switch (event.target.dataset.color) {
+        case 'black':
+            color = 'black';
+            break
+        case 'white':
+            color = 'white';
+            break
+        case 'random':
+            color = 'random';
+            break
+        case 'reset':
+            color = 'reset';
+            break
+    }
+}
+
+myBtns.forEach(colorButton => colorButton.addEventListener('click', changeColor))
 // Naming button to add event listener
 //gridButton = document.querySelector('.create-grid');
 //gridButton.addEventListener('click', createGrid)
